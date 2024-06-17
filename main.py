a588f284381e06c3502d47dc95cddbfc079b9913
Any Task <t.series@yandex.ru>
2024-06-17 23:30:08 +0300

    feat: Translate subtitle lines in main.py
    The code changes in this commit add functionality to translate subtitle lines in the `main.py` file. It introduces a new method `saveFileLines` to save the modified lines back to the file. The translation is performed using the `TextTranslator` class and the translated text is appended to the original line with a marker. The translation is done for lines that match a specific pattern.
    
    This commit also includes the addition of the `re` module and the `sleep` function from the `time` module.
    
    The purpose of this commit is to enhance the functionality of the application by providing the ability to translate subtitle lines.
    
    Note: The recent user commits and recent repository commits are not relevant for generating the commit message.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import deepl
 import pprint as pp
+import re
+from time import sleep
+
 
 class TextTranslator:
     def __init__(self) -> None:
@@ -22,17 +25,36 @@ def getFileLines() -> list:
     with open('files/snowDay.subtitlefile', 'r+', encoding='utf-8') as file:
         lines = file.readlines()
         return lines
-    
+
+def saveFileLines(lines: list) -> None:
+    with open('files/snowDay.subtitlefile', 'w+', encoding='utf-8') as file:
+        file.writelines(lines)
+
 def main() -> None:
     translator = TextTranslator()
     lines = getFileLines()
+    if lines == None:
+        return
+    
+    pattern = r"(^Dialogue:.+?,,0,0,0,,)(.+)"
     for numLine, line in enumerate(lines):
-        pp.pprint(f"Line {numLine}: {line}")
+        if not re.match(pattern, line):
+            continue
+        
+        match = re.search(pattern, line)
+        firstText = match.group(1)
+        text = match.group(2)
+        translatedText = translator.translate_text(text, "ru")
+        newLine = firstText + text + "'bigBugHere'" + translatedText + "\n"
+        lines[numLine] = newLine
+        print('Translated:', text, '->', translatedText)
+        
+        if numLine % 10 == 0:
+            print(f"Translated {numLine} lines")
+            sleep(1)
 
-
-    return
-    text = translator.translate_text("Hello, world!", "ru")
-    print(text)
+    saveFileLines(lines)
+    print("All lines translated")
 
 
 if __name__ == "__main__":
